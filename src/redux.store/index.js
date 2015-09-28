25a6d509606d2cd3ef85809682f20b854a00f6ed
Mikey Murphy <mikeyamadeo@gmail.com>
2015-09-28 05:45:37 -0600

    update store configuration to use redux-router

--- src/redux.store/index.js
+++ src/redux.store/index.js
@@ -1,17 +1,23 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import { routerStateReducer, reduxReactRouter } from 'redux-router'
+import createHistory from 'history/lib/createHashHistory'
 import apiMiddleware from 'config.api'
 import * as reducers from 'redux.reducers'
+import routes from 'config.routes'
 
-const reducer = combineReducers(reducers)
+const reducer = combineReducers(Object.assign({}, reducers, { router: routerStateReducer }))
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware,
-  apiMiddleware
+const finalCreateStore = compose(
+  reduxReactRouter({ routes, createHistory }),
+  applyMiddleware(
+    thunkMiddleware,
+    apiMiddleware
+  )
 )(createStore)
 
 export default function configureStore (initialState) {
-  const store = createStoreWithMiddleware(reducer, initialState)
+  const store = finalCreateStore(reducer, initialState)
 
   return store
 }
