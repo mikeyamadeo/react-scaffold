471e6db961100a0d6fad7603f8da974196562c95
Mikey Murphy <mikeyamadeo@gmail.com>
2015-09-28 05:45:09 -0600

    update index to work with new apis. using stateless render function

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 import React from 'react'
-import Router from 'react-router'
-import configureStore from './redux.store'
+import ReactDOM from 'react-dom'
+import configureStore from 'redux.store'
 import { Provider } from 'react-redux'
-import routes from './config.routes'
-import routing from './utils.routing'
+import { ReduxRouter } from 'redux-router'
 
 const store = configureStore()
 
-const router = Router.create({
-  routes
-})
-
-/**
- * Circumvent the circular dependency so routing functions
- * can be used outside of react components.
- */
-routing.set(router)
-
-router.run((Handler, routerState) => {
-  React.render(
-    <Provider store={store}>
-      { () => <Handler {...{routerState}}/> }
-    </Provider>,
-    document.getElementById('app')
+const Root = props => (
+    <div>
+      <Provider store={store}>
+        <ReduxRouter />
+      </Provider>
+    </div>
   )
-})
+
+ReactDOM.render(<Root />, document.getElementById('app'))
