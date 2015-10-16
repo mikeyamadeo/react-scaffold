264a12351937a6dc8aeefc0b1dabef4a84d184b9
Mikey Murphy <mikeyamadeo@gmail.com>
2015-10-16 17:10:23 -0600

    flexbox is now stateless

--- src/App/shared/obj.Flexbox/index.js
+++ src/App/shared/obj.Flexbox/index.js
@@ -3,44 +3,36 @@ import React, { PropTypes } from 'react'
 import cn from 'classnames'
 import CSSModules from 'react-css-modules'
 
-const Flexbox = React.createClass({
+const Flexbox = ({tag = 'div', children, direction, wrap, justify, align, ...props}) => {
+  const Tag = tag
+  return (
+    <Tag { ...props } styleName={cn('Flex', {
 
-  propTypes: {
-    direction: PropTypes.oneOf(['column', 'row-reverse']),
-    wrap: PropTypes.oneOf(['wrap', 'wrap-reverse']),
-    justify: PropTypes.oneOf(['end', 'center', 'space-between']),
-    align: PropTypes.oneOf(['start', 'end', 'center', 'stretch']),
-    tag: PropTypes.string,
-    children: PropTypes.oneOfType([
-      PropTypes.element,
-      PropTypes.array,
-      PropTypes.object
-    ])
-  },
+      [`Direction-${direction}`]: !!direction,
+      [`Wrap-${wrap}`]: !!wrap,
+      [`Justify-${justify}`]: !!justify,
+      [`Align-${align}`]: !!align
 
-  render () {
-    const { props } = this
-    const { tag, children,
-            direction, wrap, justify,
-            align } = props
-    const $tag = tag || 'div'
+    })}>
 
-    return (
-      <$tag { ...props } styleName={cn('Flex', {
+      { children }
 
-        [`Direction-${direction}`]: !!direction,
-        [`Wrap-${wrap}`]: !!wrap,
-        [`Justify-${justify}`]: !!justify,
-        [`Align-${align}`]: !!align
+    </Tag>
+  )
+}
 
-      })}>
-
-        { children }
-
-      </$tag>
-    )
-  }
-})
+Flexbox.propTypes = {
+  direction: PropTypes.oneOf(['column', 'row-reverse']),
+  wrap: PropTypes.oneOf(['wrap', 'wrap-reverse']),
+  justify: PropTypes.oneOf(['end', 'center', 'space-between']),
+  align: PropTypes.oneOf(['start', 'end', 'center', 'stretch']),
+  tag: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.element,
+    PropTypes.array,
+    PropTypes.object
+  ])
+}
 
 export default CSSModules(Flexbox, style, {
   allowMultiple: true,
