25cba2bebea4f7e46be32e2262d6b999b14311dd
Mikey Murphy <mikeyamadeo@gmail.com>
2015-10-21 16:49:27 -0600

    add Stack view to route

--- src/config.routes/index.js
+++ src/config.routes/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Route } from 'react-router'
+import { Route, IndexRoute } from 'react-router'
 import App from 'App'
+import Stack from 'App/views/Stack'
 
 const routes = (
   <Route path='/' component={App}>
+    <IndexRoute component={Stack} />
   </Route>
 )
 
