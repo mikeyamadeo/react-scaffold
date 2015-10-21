00113675c417808826011322bfae5dcd64013533
Mikey Murphy <mikeyamadeo@gmail.com>
2015-10-21 16:10:24 -0600

    keep App simple, demonstrating using react-router w/ children

--- src/App/index.js
+++ src/App/index.js
@@ -1,57 +1,15 @@
-import style from './style'
+import './style'
 import React, { PropTypes } from 'react'
-import CSSModules from 'react-css-modules'
-import { connect } from 'react-redux'
-import { capitalize } from 'utils.rendering'
-import { initApp } from './state/actions'
-import Flex from 'obj.Flexbox'
 
-const App = React.createClass({
+const App = ({children}) => (
+  <div>
+    { children }
+  </div>
+)
 
-  propTypes: {
-    initialize: PropTypes.func,
-    specs: PropTypes.array
-  },
-
-  componentDidMount () {
-    const { initialize } = this.props
-    initialize()
-  },
-
-  renderItems (items) {
-    return items.map((item, i) => (
-      <li key={i} styleName='list-item'>
-        { capitalize(item) }
-      </li>
-    ))
-  },
-
-  render () {
-    const { props, renderItems } = this
-    const { specs } = props
-
-    return (
-      <Flex styleName='container' direction='column' align='center'>
-        <h1 styleName='h1'>made with &#9829; and</h1>
-        <Flex tag='ul' styleName='list' direction='column' align='center'>
-          { renderItems(specs) }
-        </Flex>
-      </Flex>
-    )
-  }
-
-})
-
-export default connect(selector, boundActions)(CSSModules(App, style))
-
-function selector (state) {
-  return {
-    specs: state.appSpecs || []
-  }
+App.propTypes = {
+  children: PropTypes.node
 }
 
-function boundActions (dispatch) {
-  return {
-    initialize: () => dispatch(initApp())
-  }
-}
+export default App
+
