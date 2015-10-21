87b58efe9a6c5ebd1268d4e8364740af50d3810d
Mikey Murphy <mikeyamadeo@gmail.com>
2015-10-21 16:11:10 -0600

    remove init app action, add callApi abstraction

--- src/App/state/actions.js
+++ src/App/state/actions.js
@@ -1,17 +1,3 @@
 export const CALL_API = Symbol('Call API')
 
-export const REQUEST_SPECS = 'REQUEST_SPECS'
-
-const stackRequested = {
-  type: REQUEST_SPECS
-}
-
-/**
- * prime data here. :)
- */
-export function initApp () {
-  return (dispatch) => {
-    dispatch(stackRequested)
-    return null
-  }
-}
+export const callApi = (config) => ({ [CALL_API]: config })
