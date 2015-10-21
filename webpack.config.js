cf4f1cc0880ba364740b0aae03e6bf933d52470a
Mikey Murphy <mikeyamadeo@gmail.com>
2015-10-21 17:00:46 -0600

    clean up default html config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var appPath = path.resolve(__dirname, 'src', 'index.js')
 var stylePath = path.resolve(__dirname, 'src', 'App', 'style')
 var buildPath = path.resolve(__dirname, 'public')
 var getConfig = require('ac-webpack')
+var html = require('./html.js')
 
 /**
  * 1. Required
@@ -22,71 +23,8 @@ module.exports = getConfig({
    * index for both dev & prod.
    */
   html: {
-
-    dev: function (data) {
-      return {
-        'index.html': [
-          '<html>',
-            '<head>',
-              '<meta charset="utf-8"/>',
-              '<meta name="viewport" content="width=device-width, initial-scale=1">',
-              '<link href="http://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet" type="text/css">',
-              '<script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){' +
-              '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),' +
-              'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)' +
-              '})(window,document,"script","//www.google-analytics.com/analytics.js","ga");' +
-
-              'ga("create", "", "auto");</script>',
-            '</head>',
-            '<body>',
-              '<div id="app"></div>',
-              '<script src="/' + data.main + '"></script>',
-            '</body>',
-          '</html>'
-        ].join('')
-      }
-    },
-    prod: function (data) {
-      var config = {
-        title: 'AncestorCloud',
-        favicon: '/assets/icons/favicon.ico',
-        gaId: 'UA-47141819-4',
-        errorceptionId: '5530474fa1b3d516090018ea'
-      }
-
-      return {
-        'index.html': [
-          '<html>',
-            '<head>',
-              '<meta charset="utf-8"/>',
-              '<meta name="viewport" content="width=device-width, initial-scale=1">',
-              '<title>' + config.title + '</title>',
-              '<link rel="icon" href="' + config.favicon + '" type="image/x-icon" />',
-              '<link href="/' + data.css + '" rel="stylesheet" type="text/css" />',
-              '<link href="http://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet" type="text/css">',
-
-              '<script>(function(_,e,rr,s){_errs=[s];var c=_.onerror;_.onerror=function(){var a=arguments;_errs.push(a);' +
-              'c&&c.apply(this,a)};var b=function(){var c=e.createElement(rr),b=e.getElementsByTagName(rr)[0];' +
-              'c.src="//beacon.errorception.com/"+s+".js";c.async=!0;b.parentNode.insertBefore(c,b)};' +
-              '_.addEventListener?_.addEventListener("load",b,!1):_.attachEvent("onload",b)})' +
-              '(window,document,"script","' + config.errorceptionId + '");</script>',
-
-              '<script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){' +
-              '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),' +
-              'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)' +
-              '})(window,document,"script","//www.google-analytics.com/analytics.js","ga");' +
-
-              'ga("create", "' + config.gaId + '", "auto");</script>',
-            '</head>',
-            '<body>',
-              '<div id="app"></div>',
-              '<script src="/' + data.vendors + '"></script>',
-              '<script src="/' + data.app + '"></script>',
-            '</body>',
-          '</html>'
-        ].join('')
-      }
-    }
+    dev: html.dev,
+    prod: html.prod
   },
 
   /**
