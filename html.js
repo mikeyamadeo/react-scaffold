da4f4c9f8f2dcf87f43690f5f9c6744666bdac2e
Mikey Murphy <mikeyamadeo@gmail.com>
2016-01-30 11:21:27 -0700

    dont load fonts

--- html.js
+++ html.js
@@ -6,7 +6,6 @@ module.exports = {
           '<head>',
             '<meta charset="utf-8"/>',
             '<meta name="viewport" content="width=device-width, initial-scale=1">',
-            '<link href="http://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet" type="text/css">',
             '<script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){' +
             '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),' +
             'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)' +
@@ -39,7 +38,6 @@ module.exports = {
             '<title>' + config.title + '</title>',
             '<link rel="icon" href="' + config.favicon + '" type="image/x-icon" />',
             '<link href="/' + data.css + '" rel="stylesheet" type="text/css" />',
-            '<link href="http://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet" type="text/css">',
 
             '<script>!function(m,i,k,e,y){m.GoogleAnalyticsObject=k;m[k]||(m[k]=function(){' +
             '(m[k].q=m[k].q||[]).push(arguments)});m[k].l=+new Date;e=i.createElement("script");' +
