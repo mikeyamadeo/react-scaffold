59fb4b9921461c84cf0555af85144369ba08652f
Mikey Murphy <mikeyamadeo@gmail.com>
2016-04-09 17:04:42 -0600

    make colors accessible via js

---
+++ src/settings.style/index.js
@@ -0,0 +1,28 @@
+export const colors = {
+  blue:         '#4FC1E9',// eslint-disable-line
+  darkBlue:     '#3BAFDA',// eslint-disable-line
+
+  navy:         '#477EA8',// eslint-disable-line
+  darkNavy:     '#345C7C',// eslint-disable-line
+
+  mint:         '#48CFAD',// eslint-disable-line
+  darkMint:     '#37BC9B',// eslint-disable-line
+
+  pink:         '#EC87C0',// eslint-disable-line
+  darkPink:     '#D770AD',// eslint-disable-line
+
+  red:          '#ED5565',// eslint-disable-line
+  darkRed:      '#DA4453',// eslint-disable-line
+
+  yellow:       '#FFCE54',// eslint-disable-line
+  darkYellow:   '#F6BB42',// eslint-disable-line
+
+  grey:         '#BBBBBB',// eslint-disable-line
+  darkGrey:     '#9B9B9B',// eslint-disable-line
+  darkerGrey:   '#8C8C8C',// eslint-disable-line
+  darkestGrey:  '#555459',// eslint-disable-line
+
+  lightBase:    '#FFFFFF',// eslint-disable-line
+  base:         '#F5F5F5',// eslint-disable-line
+  darkBase:     '#D8D8D8'// eslint-disable-line
+}
