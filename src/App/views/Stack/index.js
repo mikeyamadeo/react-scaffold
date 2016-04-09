baa704beed699ea839ebbfbfdc17a4432f0ab049
Mikey Murphy <mikeyamadeo@gmail.com>
2016-04-09 17:03:25 -0600

    use aphrodite instead of css modules

--- src/App/views/Stack/index.js
+++ src/App/views/Stack/index.js
@@ -1,18 +1,18 @@
-import style from './style'
+import { StyleSheet, css } from 'aphrodite'
 import React, { PropTypes } from 'react'
-import CSSModules from 'react-css-modules'
 import { connect } from 'react-redux'
+import { colors } from 'settings.style'
 import { capitalize } from 'utils.rendering'
 import { Y } from 'obj.Layout'
 
 const Stack = ({specs}) =>
-  <Y styleName='container'>
-    <h1 styleName='h1'>
-      made with &#9829; and
+  <Y className={css(style.container)}>
+    <h1 className={css(style.h1)}>
+      made with <span className={css(style.heart)}>&#9829;</span> and
     </h1>
-    <Y tag='ul' styleName='list'>
+    <Y tag='ul' className={css(style.list)}>
       { specs.map((item, i) =>
-        <li key={i} styleName='list-item'>
+        <li key={i} className={css(style.listItem)}>
           { capitalize(item) }
         </li>
       )}
@@ -25,4 +25,34 @@ Stack.propTypes = {
 
 export default connect(state => ({
   specs: state.appSpecs
-}))(CSSModules(Stack, style))
+}))(Stack)
+
+const style = StyleSheet.create({
+  container: {
+    width: '100%',
+    minHeight: '100%'
+  },
+  h1: {
+    width: '100%',
+    margin: 0,
+    padding: '15px 0',
+    minHeight: '70px',
+    textAlign: 'center',
+    backgroundColor: 'white'
+  },
+  list: {
+    listStyle: 'none',
+    padding: '20px 0'
+  },
+  listItem: {
+    fontSize: '2em',
+    marginTop: '10px',
+    textAlign: 'center',
+    '@media (min-width: 31.25em)': {
+      fontSize: '3em'
+    }
+  },
+  heart: {
+    color: colors.red
+  }
+})
