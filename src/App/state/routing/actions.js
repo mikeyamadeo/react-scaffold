b0775ce7e41b9fdb90a722cf19d52ab8bee2cd85
Mikey Murphy <mikeyamadeo@gmail.com>
2016-04-09 16:52:04 -0600

    use react-router-redux instead of redux-router

--- src/App/state/routing/actions.js
+++ src/App/state/routing/actions.js
@@ -2,14 +2,14 @@
   These serve as convenience abstractions over the base redux router actions
 \* ------------------------------------------------------------------------ */
 
-import { pushState, replaceState } from 'redux-router'
+import { push, replace } from 'react-router-redux'
 
 /**
  * @param {String} pathname - e.g. '/search/users'
  * @param {Object} query - e.g. { key0: value, key1: value }
  */
-export const transitionTo = (pathname, query = {}) => pushState(null, pathname, query)
-export const replaceWith = (pathname, query = {}) => replaceState(null, pathname, query)
+export const transitionTo = (pathname, query = {}) => push(null, pathname, query)
+export const replaceWith = (pathname, query = {}) => replace(null, pathname, query)
 
 /**
  * Depends on redux-thunk to get the current pathname from the state
