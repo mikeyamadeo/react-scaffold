fd1da9e502b83b5b1dbfb375f5a0c990fa5d8277
James <james389205+git@gmail.com>
2016-05-16 13:24:46 -0600

    Update Ava

--- src/App/state/routing/spec.js
+++ src/App/state/routing/spec.js
@@ -1,20 +1,9 @@
 import test from 'ava'
 import { transitionTo, replaceWith, updateQuery, replaceQuery } from './actions'
 
-test('transitionTo action tests', t => {
-  const action = transitionTo('/test')
-  t.is(action.payload.method, 'pushState')
-  t.same(action.payload.args, [null, '/test', {}])
-})
+const dispatch = (test) => test
 
-test('replaceWith action tests', t => {
-  const action = replaceWith('/test')
-  t.is(action.payload.method, 'replaceState')
-  t.same(action.payload.args, [null, '/test', {}])
-})
-
-const dispatch = _ => _
-const getState = _ => ({
+const getState = () => ({
   router: {
     location: {
       pathname: '/test'
@@ -22,14 +11,26 @@ const getState = _ => ({
   }
 })
 
-test('updateQuery action tests', t => {
-  const action = updateQuery({test: 1})(dispatch, getState)
-  t.is(action.payload.method, 'pushState')
-  t.same(action.payload.args, [null, '/test', {test: 1}])
+test('transitionTo action tests', (t) => {
+  const action = transitionTo('/test')
+  t.is(action.payload.method, 'push')
+  t.deepEqual(action.payload.args, [null, '/test', {}])
 })
 
-test('replaceQuery action tests', t => {
+test('replaceWith action tests', (t) => {
+  const action = replaceWith('/test')
+  t.is(action.payload.method, 'replace')
+  t.deepEqual(action.payload.args, [null, '/test', {}])
+})
+
+test('updateQuery action tests', (t) => {
+  const action = updateQuery({test: 1})(dispatch, getState)
+  t.is(action.payload.method, 'push')
+  t.deepEqual(action.payload.args, [null, '/test', {test: 1}])
+})
+
+test('replaceQuery action tests', (t) => {
   const action = replaceQuery({test: 1})(dispatch, getState)
-  t.is(action.payload.method, 'replaceState')
-  t.same(action.payload.args, [null, '/test', {test: 1}])
+  t.is(action.payload.method, 'replace')
+  t.deepEqual(action.payload.args, [null, '/test', {test: 1}])
 })
